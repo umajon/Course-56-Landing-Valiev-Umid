04cca48cd88033082041e942985aeb0b3e4bd483
umajon <v.umajon@gmail.com>
2024-09-21 22:50:48 +0500

    page5+timer2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ function correctDate(date) {
     return date < 10 ? "0" + date : date;
 }
 
-
-
 function sale() {
     let saleDate = new Date(2024, 8, 25)
     let currentDAte = new Date()
@@ -17,11 +15,19 @@ function sale() {
     timeValue[1].innerHTML = correctDate(hours)
     timeValue[2].innerHTML = correctDate(minutes)
     timeValue[3].innerHTML = correctDate(seconds)
+    timeValue[4].innerHTML = correctDate(day)
+    timeValue[5].innerHTML = correctDate(hours)
+    timeValue[6].innerHTML = correctDate(minutes)
+    timeValue[7].innerHTML = correctDate(seconds)
     if (date <= 0) {
         timeValue[0].innerHTML = "00"
         timeValue[1].innerHTML = "00"
         timeValue[2].innerHTML = "00"
         timeValue[3].innerHTML = "00"
+        timeValue[4].innerHTML = "00"
+        timeValue[5].innerHTML = "00"
+        timeValue[6].innerHTML = "00"
+        timeValue[7].innerHTML = "00"
     }
 }
 sale();
